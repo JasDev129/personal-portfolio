19a5beeb020024d89f1c88394a27d63533a88bcf
xyper <xyper.gmail>
2022-10-13 16:49:42 +0800

    Nav toggle done!

--- main.js
+++ main.js
@@ -6,10 +6,12 @@ let navStatus = true;
         
         if(navStatus == true){
             navList.style.opacity = "1";
+            navList.style.visibility = "visible";
             navToggle.style.transform = "rotate(180deg)";
             navStatus = false;
         }else if(navStatus == false){
             navList.style.opacity = "0";
+            navList.style.visibility = "hidden";
             navToggle.style.transform = "rotate(0deg)";
             navStatus = true;
         }
