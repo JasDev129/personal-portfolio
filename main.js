11bca5dc1c2e5bc686cad7bc5d5045c0aac0d54a
xyper <xyper.gmail>
2022-10-13 19:57:35 +0800

    Media Queries & Adding some animation

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// FOR NAVIGATION BAR
 const navToggle = document.querySelector('.nav-toggle');
 const navList = document.querySelector('.nav-list');
 let navStatus = true;
@@ -15,3 +16,50 @@ let navStatus = true;
           navStatus = true;
         }
     });
+
+// FOR HERO CONTENT
+
+    const ctaButton = document.querySelector(".cta-btn");
+    const heroContainer = document.querySelector('.hero-container');
+    const mediaQueryM = window.matchMedia("(min-width: 768px)");
+    const profileDiv = document.querySelector(".myprofile-pic");
+    const heroContent = document.querySelector(".hero-content");
+    const ctaDiv = document.querySelector(".cta");
+    const profileBg = document.querySelector('.profile-background');
+    const heroPara = document.querySelector(".hero-para");
+    const heroHeader = document.querySelector(".hero-header");
+    let showAbout = true;
+            ctaButton.addEventListener("click", () => {
+                if (showAbout == true) {
+                  if (mediaQueryM.matches) {
+                    console.log("min-width: 768px / TRUE");
+                    heroContainer.style.display = "grid";
+                    heroContainer.style.gridTemplateColumns = "repeat(4, 1fr)";
+                    heroContainer.style.gridTemplateRows = "repeat(4, 1fr)";
+                    heroContainer.style.placeItems = "center";
+                   
+                    profileBg.style.transform = 'rotate(-130deg)';
+                    // hero div's
+                    profileDiv.style.gridColumn = "1 / span 2";
+                    profileDiv.style.gridRow = "2";
+                    heroContent.style.gridColumn = '1 / span 2'
+                    heroContent.style.gridRow = "3";
+                    heroPara.style.fontSize = "1.2rem";
+                    heroHeader.style.fontSize = "1.5rem";
+                    ctaDiv.style.gridColumn = "1 / span 2";
+                    ctaDiv.style.gridRow = "4";
+                    showAbout = false;
+                  }
+                }else if(showAbout == false){
+                    console.log("min-width: 768px / FALSE");
+                    heroContainer.style.display = "flex";
+                     profileBg.style.transform = "rotate(130deg)";
+                     // hero div's
+                     heroPara.style.fontSize = "1.7rem";
+                     heroHeader.style.fontSize = "2rem";
+                     ctaDiv.style.gridColumn = "1 / span 2";
+                     ctaDiv.style.gridRow = "4";
+                    showAbout = true;
+                }
+            });
+      
