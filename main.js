1525825dcc6f729bd33920bf05d901173fd2f69f
xyper <xyper.gmail>
2022-10-13 17:39:58 +0800

    Fixing style and adding some features

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navList = document.querySelector('.nav-list');
-
 let navStatus = true;
     navToggle.addEventListener('click', () => {
         
-        if(navStatus == true){
-            navList.style.opacity = "1";
-            navList.style.visibility = "visible";
-            navToggle.style.transform = "rotate(180deg)";
-            navStatus = false;
-        }else if(navStatus == false){
-            navList.style.opacity = "0";
-            navList.style.visibility = "hidden";
-            navToggle.style.transform = "rotate(0deg)";
-            navStatus = true;
+        if (navStatus == true) {
+          navList.style.opacity = "1";
+          navList.style.visibility = "visible";
+          navToggle.style.transform = "rotate(180deg)";
+          navStatus = false;
+        } else if (navStatus == false) {
+          navList.style.opacity = "0";
+          navList.style.visibility = "hidden";
+          navToggle.style.transform = "rotate(0deg)";
+          navStatus = true;
         }
     });
